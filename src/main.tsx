6770d51f1e84a075429c14770811cfaf4144fdab
dangphu2412 <dangphu241299@gmail.com>
2025-09-03 21:30:25 +0700

    feat: setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './main.css';
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router";
+import {lazy} from "react";
+import {Home} from "./Home.tsx";
+
+const Test1 = lazy(async () => import('./test1/Test1.tsx'))
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/test1" element={<Test1 />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>,
+
 )
---
+++ src/test1/Test1.tsx
@@ -0,0 +1,5 @@
+export default function Test1() {
+    return <div>
+        Hello world
+    </div>
+}
